2c064e3101c46bb33ebd42e5efd9a72e154e9839
ofthewildfire <103148299+ofthewildfire@users.noreply.github.com>
2024-06-20 10:27:02 +0200

    idk this might be a rollback point.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "@fontsource-variable/playfair-display";
 import DataType from "./interfaces/DataInterface";
 import data from "./data.ts";
+// import ThemeButton from "./components/ThemeButton.tsx";
 import Header from "./components/Header.tsx";
 import PostCard from "./components/PostCard.tsx";
 import Footer from "./components/Footer.tsx";
@@ -16,6 +17,7 @@ function App() {
   return (
     <>
       <main className=" main-content bg-[#f9f9f9]">
+        {/* <ThemeButton /> */}
         <Header />
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8 px-2 py-8">
           {travelElements}
